2d03fa03556c5f345ddd217a44f282231600d038
Emily Ranson <ransonme@tcd.ie>
2018-01-22 12:35:07 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = new Object({artistName: "songTitle"});
 
-Object.assign({}, {'Phil Ochs', "Here's to the State of Mississippi"})
+Object.assign({}, {'Phil Ochs': "Here's to the State of Mississippi"});
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  return Object.assign({}, )
+  return Object.assign({})
 }
