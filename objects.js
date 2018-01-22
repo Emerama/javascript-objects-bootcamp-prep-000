08ae852cae363f1be0644940262260e9fa5e5496
Emily Ranson <ransonme@tcd.ie>
2018-01-22 13:12:07 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,7 @@
-var playlist = new Object(
-  {'Fall Out Boy': 'Champion'}
-);
+var playlist = {
+  'Fall Out Boy': 'Champion',
+  'Imagine Dragons': 'Round and Round'
+};
 
 Object.assign({}, {'Phil Ochs': "Here's to the State of Mississippi"});
 
