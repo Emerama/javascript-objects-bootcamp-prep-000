34d41da706830ec0633e472251e151d002db359a
Emily Ranson <ransonme@tcd.ie>
2018-01-22 11:05:47 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,15 @@
-Object.assign({}, {foo: 'bar'})
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
+  obj[key] = value
+ 
+  return obj
+};
+ 
+const recipe = { eggs: 3 };
+ 
+destructivelyUpdateObjectWithKeyAndValue(recipe, 'flour', '3 cups');
 
-Object.assign({eggs: 3}, {flour: '1 cup'})
+Object.assign({}, {foo: 'bar'});
 
-Object.assign({eggs:3}, {chocolate: '1 cup', flour: '2 cups'}, {flour: '1/2 cup'})
+Object.assign({eggs: 3}, {flour: '1 cup'});
+
+Object.assign({eggs:3}, {chocolate: '1 cup', flour: '2 cups'}, {flour: '1/2 cup'});
