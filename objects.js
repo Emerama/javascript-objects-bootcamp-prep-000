fbbed99925d564a396072b9fa2329cb3946f1d55
Emily Ranson <ransonme@tcd.ie>
2018-01-22 13:15:49 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -10,7 +10,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
 }
 
 function removeFromPlaylist(playlist, artistName) {
-  delete playlist['Kanye'] = "Gold Digger";
+  delete playlist.Kanye = "Gold Digger";
   
   return playlist
 }
